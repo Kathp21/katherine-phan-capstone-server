52d214baa87aa99a002606c33d2e8e18aa1008d0
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-02-25 23:10:11 -0800

    Add codes

---
+++ routes/recommendations.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const router = express.Router()
+const axios = require("axios")
+
+router.post('/', async (req, res) => {
+    try {
+        console.log(req.body)
+        const { destination, season, duration, budget, interests, additionalInfo } = req.body
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+            model: "gpt-3.5-turbo-1106",
+            messages: [{ 
+                role: 'user',
+                content: `I would like a travel itinerary with the following details:
+                Destination: ${destination} 
+                Season: ${season}
+                Duration: ${duration}
+                Budget: ${budget} in Canadian dollar with dollar sign. Show budget for each day.
+                Interests: ${interests}
+                Additional Info: ${additionalInfo}
+
+                Provide the output in JSON with the daily recommendations as elements
+                in an array. Each element should be an object with the following schema:
+
+                {
+                    "itinerary": [
+                        {
+                            "day_string": <A string that represents the day of the trip>,
+                            "location": <A more specific area within the destination>,
+                            "duration": <string>,
+                            "budget": <A dollar amount with the dollar sign. The budget required for activities for this specific day>,
+                            "description": <A description of the activities that you'll be doing>
+                        }
+                    ]
+                }
+
+                `
+            }], 
+            response_format: { "type": "json_object" }
+        }, {
+            headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer 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`
+            }
+        });
+        const responseData = response.data.choices[0].message.content
+        res.status(200).json(responseData)
+        console.log(responseData)
+    }  catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const cors = require('cors')
+require('dotenv').config()
+const dataRoutes = require('./routes/recommendations')
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/chat-completion', dataRoutes)
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
